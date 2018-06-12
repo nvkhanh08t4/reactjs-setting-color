3b65d1c4073d7c241b3ffe75f3a7fefec89a4604
Khanh Nguyen <nvkhanh08t4@gmail.com>
2018-06-12 22:13:41 +0700

    Create components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ColorPicker from './components/ColorPicker';
+import SizeSetting from './components/SizeSetting';
+import Reset from './components/Reset';
+import Result from './components/Result';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="row mt-50">
+          <ColorPicker />
+          <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <SizeSetting />
+            <Reset />
+          </div>
+          <Result />
+        </div>
       </div>
     );
   }
---
+++ src/components/Reset.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Reset extends Component {
+  render() {
+    return (
+        <button type="button" class="btn btn-info">Reset</button>
+    );
+  }
+}
+
+export default Reset;
